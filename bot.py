38849ff4243aa5feaf9dfef3c89f59e05dede763
Tony <cardiox1212@hotmail.fr>
2020-11-17 08:46:47 +0100

    Update bot.py to manage other languages

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@
 #    By: bbellavi <bbellavi@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/11/16 06:16:50 by bbellavi          #+#    #+#              #
-#    Updated: 2020/11/17 06:31:41 by bbellavi         ###   ########.fr        #
+#    Updated: 2020/11/17 08:42:43 by bbellavi         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -20,8 +20,31 @@ CWD				= os.getcwd()
 POLL_DIR		= "poll"
 EXEC_ROLE		= "execute"
 EXEC_CHANNELS 	= [
-	"777764433761271828"	
+	"777764433761271828",
+	"778144414437867533",
+	"778144436138934312",
+	"778144451851583498",
+	"778144467748257852"
 ]
+
+CONTEXT			= {
+	"py" : {
+		"image"			: "python:3",
+		"interpreter"	: "python"
+	},
+	"sh" : {
+		"image"			: "bash",
+		"interpreter"	: "bash",
+	},
+	"js" : {
+		"image"			: "node",
+		"interpreter"	: "node"
+	},
+	"php" : {
+		"image"			: "php:7.4-cli",
+		"interpreter"	: "php"
+	}
+}
 
 client = discord.Client()
 
@@ -29,9 +52,16 @@ class TheExecutor(discord.Client):
 	async def on_ready(self):
 		print(f"{self.user} has connected to Discord!")
 
-	def execute(self, filename):
-		COMMAND = f"docker run -it --rm -v {CWD}/{POLL_DIR}:/tmp/{POLL_DIR} -w /tmp/{POLL_DIR} python:3 python {filename}"
-		print(COMMAND)
+	def get_command(self, filename, context):
+		return f"docker run -it --rm -v " \
+				f"{CWD}/{POLL_DIR}:/tmp/{POLL_DIR} " \
+				f"-w /tmp/{POLL_DIR} " \
+				f"{context['image']} " \
+				f"{context['interpreter']} {filename}"
+
+	def execute(self, filename, context):
+		COMMAND = self.get_command(filename, context)
+		
 		try:
 			process = subprocess.run(COMMAND.split(), capture_output=True, timeout=10)
 			if process.returncode != 0:
@@ -49,23 +79,28 @@ class TheExecutor(discord.Client):
 
 		user_roles = [role.name for role in message.author.roles]
 
+
 		if str(message.channel.id) in EXEC_CHANNELS and EXEC_ROLE in user_roles:
-			if message.content.startswith("```py") and message.content.endswith("```"):
-				content = message.content[5:-3]
-				content = content.strip()
-				
-				msg_hash = hashlib.sha256(f"{message.content + str(message.id)}".encode()).hexdigest()
-				filename = f"{msg_hash}.py"
-				fullname = f"{POLL_DIR}/{filename}"
-				
-				with open(fullname, 'w') as f:
-					f.write(content)
+			if message.content.startswith("```") and message.content.endswith("```"):
+				extension = message.content[3:][:3].strip()
 
-				result = self.execute(filename)
+				if extension in CONTEXT.keys():
+					content = message.content[5:-3].strip()
+					
+					msg_hash = hashlib.sha256(f"{message.content + str(message.id)}".encode()).hexdigest()
+					filename = f"{msg_hash}.{extension}"
+					fullname = f"{POLL_DIR}/{filename}"
+					
+					with open(fullname, 'w') as f:
+						f.write(content)
 
-				os.remove(fullname)
+					result = self.execute(filename, CONTEXT[extension])
 
-				await message.channel.send(f"```md\n{result}\n```")
+					
+
+					os.remove(fullname)
+
+					await message.channel.send(f"```md\n{result}\n```")
 
 if TOKEN is not None:
 	TheExecutor().run(TOKEN)
