245623da12609285f8c02936fdbf9c103a2aa1dd
Tony <cardiox1212@hotmail.fr>
2020-11-17 02:57:31 +0100

    Add execution role protection and refactor on_message

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@
 #    By: bbellavi <bbellavi@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/11/16 06:16:50 by bbellavi          #+#    #+#              #
-#    Updated: 2020/11/16 08:33:39 by bbellavi         ###   ########.fr        #
+#    Updated: 2020/11/17 02:57:05 by bbellavi         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -15,22 +15,41 @@ import hashlib
 import discord
 import subprocess
 
-EXEC_CHANNEL = "777764433761271828"
+TOKEN			= os.environ.get('DISCORD_TOKEN')
+CWD				= os.getcwd()
+POLL_DIR		= "poll"
+EXEC_ROLE		= "execute"
+EXEC_CHANNELS 	= [
+	"777764433761271828"	
+]
 
-TOKEN = os.environ.get('DISCORD_TOKEN')
-CWD = os.getcwd()
-POLL_DIR = "poll"
 client = discord.Client()
 
 class TheExecutor(discord.Client):
 	async def on_ready(self):
 		print(f"{self.user} has connected to Discord!")
 
+	def execute(self, filename):
+		COMMAND = f"docker run -it --rm -v {CWD}/{POLL_DIR}:/tmp/{POLL_DIR} -w /tmp/poll python:3 python {filename}"
+
+		try:
+			process = subprocess.run(COMMAND.split(), capture_output=True, timeout=10)
+			if process.returncode != 0:
+				result = "Error : " + process.stderr.decode() if process.stderr else process.stdout.decode()
+			else:
+				result = process.stdout.decode()
+		except subprocess.TimeoutExpired:
+			result = "Time limit exceeded"
+		
+		return result
+
 	async def on_message(self, message):
 		if not os.path.exists(POLL_DIR):
 			os.mkdir(POLL_DIR)
 
-		if str(message.channel.id) == EXEC_CHANNEL:
+		user_roles = [role.name for role in message.author.roles]
+
+		if str(message.channel.id) in EXEC_CHANNELS and EXEC_ROLE in user_roles:
 			if message.content.startswith("```py") and message.content.endswith("```"):
 				content = message.content[5:-3]
 				content = content.strip()
@@ -41,17 +60,8 @@ class TheExecutor(discord.Client):
 				
 				with open(fullname, 'w') as f:
 					f.write(content)
-				
-				COMMAND = f"docker run -it --rm -v {CWD}/{POLL_DIR}:/tmp/{POLL_DIR} -w /tmp/poll python:3 python {filename}"
 
-				try:
-					process = subprocess.run(COMMAND.split(), capture_output=True, timeout=10)
-					if process.returncode != 0:
-						result = "Error : " + process.stderr.decode() if process.stderr else process.stdout.decode()
-					else:
-						result = process.stdout.decode()
-				except subprocess.TimeoutExpired:
-					result = "Time limit exceeded"
+				result = self.execute(filename)
 
 				os.remove(fullname)
 
